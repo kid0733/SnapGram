05c95be6b68a75cc7ae237e837396cc541437441
Siddhant Singh Karki <siddhantskarki@gmail.com>
2024-02-28 11:47:16 +1100

    authFunctionality

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -8,6 +8,7 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import { Loader } from "lucide-react"
 import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
 
 const SignupForm = () => {
@@ -24,10 +25,10 @@ const SignupForm = () => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log('values')
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser= await createUserAccount(values);
+
+    console.log(newUser);
   }
 
   return (
