37b5a0542cd5c8731a5da502c13141254640139b
Siddhant PC <siddhantskarki@gmail.com>
2024-03-02 21:01:29 +1100

    'Homepage'

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import {Routes, Route} from 'react-router-dom'
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import AuthLayout from './_auth/AuthLayout'
-import { Home } from './_root/pages'
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages'
 import RootLayout from './_root/RootLayout'
 import { Toaster } from './components/ui/toaster'
 import './globals.css'
+import { Save } from 'lucide-react'
 
 const App = () => {
   return (
@@ -21,6 +22,14 @@ const App = () => {
         {/*private routes*/}
         <Route element={<RootLayout />}>
           <Route index element={<Home />}/>
+          <Route path="/explore" element={<Explore />}/>
+          <Route path="/saved" element={<Saved />}/>
+          <Route path="/all-users" element={<AllUsers />}/>
+          <Route path="/create-post" element={<CreatePost />}/>
+          <Route path="/update-post/:id" element={<EditPost />}/>
+          <Route path="/posts/:id" element={<PostDetails />}/>
+          <Route path="/profile/:id/*" element={<Profile />}/>
+          <Route path="/update-profile/:id" element={<UpdateProfile />}/>
         </Route>
       </Routes>
       <Toaster/>
---
+++ src/components/shared/Leftsidebar.tsx
@@ -0,0 +1,64 @@
+
+import { useEffect } from 'react'
+import { Link,NavLink, useNavigate, useLocation} from 'react-router-dom'
+import { Button } from '../ui/button'
+import { useSignOutAccount } from '@/lib/react-query/queritesAndMutation'
+import { useUserContext } from '@/context/AuthContext'
+import { sidebarLinks } from '@/constants'
+import { INavLink } from '@/types'
+
+const Leftsidebar= () => {
+    const {mutate: signOut, isSuccess}=useSignOutAccount()
+    const navigate=useNavigate();
+    const {user}=useUserContext()
+    const {pathname}=useLocation()
+    useEffect(()=>{
+        if(isSuccess) navigate(0)
+    },[isSuccess])
+
+  return (
+    <nav className="leftsidebar">
+        <div className="flex flex-col gap-11">
+            <Link to="/" className="flex gap-3 items-center justify-center">
+                <img src="assets/images/logo.png" alt="logo" width={38}/>
+            </Link>
+            <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
+                <img src={user.imageUrl || '(.)-(.)'} alt="profile" className='h-8 w-8 rounded-full' />
+                <div className="flex flex-col">
+                    <p className='body-bold'>
+                        {user.name}
+                    </p>
+                    <p className='small-regular text-light-4'>@ {user.username}</p>
+                </div>
+            </Link>
+            
+            <ul className="flex flex-col gap-6">
+                {sidebarLinks.map((link:INavLink)=>{
+                    const isActive=pathname===link.route;
+                    return (
+                        <li key={link.label} className={`leftsidebar-link group ${
+                            isActive && 'bg-primary-500'
+                        
+                        
+                        }`}>
+                            <NavLink to={link.route} className="flex gap-4 items-center p-4" >
+                                <img src={link.imgURL} alt={link.label} className={`group-hover:invert-white ${
+                                    isActive && 'invert-white'
+                                }`}/>
+                                {link.label}
+                            </NavLink>
+
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+        <Button variant="ghost" className='shad-button_ghost' onClick={()=>{signOut()}}>
+            <img src="assets/icons/logout.svg" alt="log out" />
+            <p className='small-medium lg:base-medium'>Log out</p>
+        </Button>
+    </nav>
+  )
+}
+
+export default Leftsidebar
